fef36ecb8381429837927f7c50aa113797d570cc
a0c6h1 <Omar.Ceesay@bluecrossmn.com>
2017-01-27 10:07:38 -0600

    File upload works( files are not linked to an account | just stay in DB)

--- app/models/file.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var fileSchema = mongoose.Schema({
-	localFile: {
-		filename: String
-	}
-});
-
-module.exports = mongoose.model('File', fileSchema);
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -5,6 +5,8 @@ var dbUrl = 'mongodb://oceesay:oman531999@ds117919.mlab.com:17919/oc_node_db';
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var fs = require('fs');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 module.exports = function(router, passport){
 
@@ -80,24 +82,42 @@ module.exports = function(router, passport){
 
   });
 
-	router.post('/upload', function(req, res){
-		console.log("------- "+req.body.file);
+	router.post('/upload', upload.single('file'), function(req, res){
 
-		mongo.MongoClient.connect(dbUrl, function(error, db) {
-		  assert.ifError(error);
+		var file = '/' + req.file.filename;
+		fs.readFile( req.file.path, function (err, data) {
+			fs.writeFile(file, data, function (err) {
+			 if( err ){
+						console.error( err );
+						response = {
+								 message: 'Sorry, file couldn\'t be uploaded.',
+								 filename: req.file.originalname
+						};
+			 }else{
+						 response = {
+								 message: 'File uploaded successfully',
+								 filename: req.file.originalname
+						};
+				}
+				mongo.MongoClient.connect(dbUrl, function(error, db) {
+				  assert.ifError(error);
 
-		  var bucket = new mongo.GridFSBucket(db);
-			fs.createReadStream(req.body.file).
-		    pipe(bucket.openUploadStream('/'+req.body.file)).
-		    on('error', function(error) {
-		      assert.ifError(error);
-		    }).
-		    on('finish', function() {
-		      console.log('done!');
-		      process.exit(0);
-					res.redirect('/auth/profile');
-		    });
-		});
+				  var bucket = new mongo.GridFSBucket(db);
+					fs.createReadStream("./uploads/"+req.file.filename).
+				    pipe(bucket.openUploadStream(req.file.filename)).
+				    on('error', function(error) {
+				      assert.ifError(error);
+				    }).
+				    on('finish', function() {
+				      console.log('done!');
+							res.redirect('/auth/profile');
+							fs.unlinkSync("C:/Users/A0C6H1/git/node-auth-signup/uploads/"+req.file.filename);
+				    });
+				});
+			 });
+		 });
+
+
 	});
 
 
