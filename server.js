6c040a8f33fb9dbbb064768b03b69db98039bf29
Omar Ceesay <oman531999@gmail.com>
2017-06-20 10:12:36 -0500

    Fix

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var port = process.env.PORT || 8080;
-var dbUrl = 'mongodb://localhost/ReactApp';
-//var dbUrl = 'mongodb://oceesay:oman531999@ds117919.mlab.com:17919/oc_node_db';
+//var dbUrl = 'mongodb://localhost/ReactApp';
+var dbUrl = 'mongodb://oceesay:oman531999@ds117919.mlab.com:17919/oc_node_db';
 //var client = require('twilio')('AC9c5006526a870fb2023c8247cdf5081d', 'b188e46e390578f26e9ba7efb30ad38a');
 
 var cookieParser = require('cookie-parser');
