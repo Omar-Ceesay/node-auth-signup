7c15ab2b21bcd3a105a27ec0eb8b0aaae59265fd
Omar Ceesay <oman531999@gmail.com>
2017-06-20 14:05:14 -0500

    fix

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -87,11 +87,11 @@ module.exports = function(router, passport){
 		s3.listObjectsV2(params, function(err, data) {
 			if (err) console.log(err, err.stack); // an error occurred
 			else{
-				console.log(data.Contents[0]);
 				imgData = data.Contents[0];
+				console.log(imgData);
+				res.render('profile.ejs', { user: req.user, pic: imgData});
 			}
 		});
-			res.render('profile.ejs', { user: req.user, pic: imgData});
 
   });
 
