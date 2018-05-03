115a7d85f36c1675f14cec6744cec01a8aca8f9f
DESKTOP-JB2RS2I\OmarC <oman531999@gmail>
2018-05-03 11:48:46 -0500

    no message

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -133,10 +133,15 @@ module.exports = function(router, passport){
 										if( err ){
 											console.error( err );
 										}else{
-											res.download( __dirname+"/temp/test.txt");
+											res.download( __dirname+"/temp/test.txt", "test.txt", function(err){
+												if(err){
+													console.log(err)
+												}else{
+													res.render('profile.ejs', { user: req.user, files: files});
+												};
+											});
+											// fs.unlinkSync(tempFile);
 											// res.download(tempFile);
-											fs.unlinkSync(tempFile);
-											res.render('profile.ejs', { user: req.user, files: files});
 
 										}
 									});
